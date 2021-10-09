60bdf324166bb0df8b9037874be3b48806ee1258
Ayush <sharma.as.ayush@gmail.com>
2021-10-09 18:30:40 +0530

    testing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("sjdhvjs");
+        System.out.println("CoWin Portal initialised...");
+        System.out.println("------------------------------");
+        System.out.println("1. Add Vaccine");
+        System.out.println("2. Register Hospital");
+        System.out.println("3. Register Citizen");
+        System.out.println("4. Add Slot for Vaccination");
+        System.out.println("5. Book Slot for Vaccination");
+        System.out.println("6. List all slots for a hospital");
+        System.out.println("7. Check Vaccination Status");
+        System.out.println("8. Exit");
     }
 }
